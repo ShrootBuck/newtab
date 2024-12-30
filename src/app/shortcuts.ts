db686f83e87be9cb293b086f34e563e118c8c867
ShrootBuck <ShrootBuck@users.noreply.github.com>
2024-12-29 22:41:14 -0700

    Added shortcuts

---
+++ src/app/shortcuts.ts
@@ -0,0 +1,68 @@
+export type Shortcut = {
+  name: string;
+  url: string;
+  iconUrl: string;
+};
+
+export const shortcuts: Shortcut[] = [
+  {
+    name: "YouTube",
+    url: "https://youtu.be",
+    iconUrl: "youtube.svg",
+  },
+  {
+    name: "AWS",
+    url: "https://console.aws.amazon.com",
+    iconUrl: "aws.svg",
+  },
+  {
+    name: "Plane",
+    url: "https://app.plane.so",
+    iconUrl: "plane.svg",
+  },
+  {
+    name: "Lichess",
+    url: "https://lichess.org",
+    iconUrl: "lichess.svg",
+  },
+  {
+    name: "GitHub",
+    url: "https://github.com",
+    iconUrl: "github.svg",
+  },
+  {
+    name: "ChatGPT",
+    url: "https://chatgpt.com",
+    iconUrl: "openai.svg",
+  },
+  {
+    name: "Proton Drive",
+    url: "https://drive.proton.me",
+    iconUrl: "protondrive.svg",
+  },
+  {
+    name: "Khan Academy",
+    url: "https://khanacademy.org",
+    iconUrl: "khanacademy.svg",
+  },
+  {
+    name: "Hacker News",
+    url: "https://news.ycombinator.com",
+    iconUrl: "ycombinator.svg",
+  },
+  {
+    name: "Exercism",
+    url: "https://exercism.org",
+    iconUrl: "exercism.svg",
+  },
+  {
+    name: "Stripe",
+    url: "https://stripe.com",
+    iconUrl: "stripe.svg",
+  },
+  {
+    name: "TenByte",
+    url: "https://tenbyte.org/ghost",
+    iconUrl: "ghost.svg",
+  },
+];
