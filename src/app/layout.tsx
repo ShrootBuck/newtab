8a899c49e13ac05bc172344490137d5332aef44f
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-02-01 12:01:31 -0700

    Remove Deepseek

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="dark">
           {children}
         </ThemeProvider>
       </body>
