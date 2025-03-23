6948421a46bd43a02bc3df551640d07191bc9dc7
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-03-23 14:51:17 -0700

    Update Walmart logo and fix hydration warning
    Fix hydration warning in layout, simplify Walmart SVG logo, and
    change AP Classroom shortcut to College Board

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${inter.variable}`}>
+    <html lang="en" className={`${inter.variable}`} suppressHydrationWarning>
       <body>
         <ThemeProvider 
           attribute="class" 
