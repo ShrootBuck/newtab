b2a530cd632cdfce7d250955ea9b48733187b5dc
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-06-08 23:32:23 +0300

    Enable custom Cache-Control headers for all routes

--- next.config.js
+++ next.config.js
@@ -6,19 +6,20 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-  // async headers() {
-  //   return [
-  //     {
-  //       source: '/(.*)',
-  //       headers: [
-  //         {
-  //           key: 'Cache-Control',
-  //           value: 'public, max-age=604800, stale-while-revalidate=86400',
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Apply to all routes
+        headers: [
+          {
+            key: 'Cache-Control',
+            // 24 hours in seconds = 24 * 60 * 60 = 86400
+            value: 'public, max-age=86400, stale-while-revalidate=86400',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default config;
